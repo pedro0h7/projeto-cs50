52a2c67f98a1fe2609df25c45d786d8d847ec2d8
Pedro Henrique <pedrohenriquedossantos0999@gmail.com>
2026-08-06 20:31:38 -0300

    feat. Sockect.io integrated into the server and client connecting

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
 const port = 3000;
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+io.on('connection', (socket) => {
+    console.log("Player connected: " + socket.id);
+
+    socket.on('disconnect', () => {
+        console.log("Player disconnected: " + socket.id);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
