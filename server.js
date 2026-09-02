69db6604059e0c2ae7f425ecd256827c5d300ac8
Pedro Henrique <pedrohenriquedossantos0999@gmail.com>
2026-09-02 20:37:10 -0300

    fix: Server listening in 0.0.0.0 for deploy.

--- server.js
+++ server.js
@@ -119,6 +119,6 @@ setInterval(() => {
     }
 }, 3000);
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
